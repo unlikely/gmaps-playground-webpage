d2ffd9777e12742657f7e94beda997eb625ed106
Patrick Kennedy <bigredtriangle@gmail.com>
2014-06-11 16:18:01 -0400

    draw rectangle to hide array elements

--- maps.js
+++ maps.js
@@ -1,5 +1,6 @@
 
 var site_location = {lat: 41.559469, lon: (-87.669026)}
+var mrg = []
 
 function doMap(siteLocation) {
   var mapOptions = {
@@ -23,7 +24,8 @@ function drawThing() {
   shapesDrawer.drawBoxes(rw,cls)
 }
 
-shapesDrawer = {
+// todo: rewrite this to use function-that-returns-object style
+var shapesDrawer = {
     drawBoxes: function(rows, cols) {
       boxNum = 0;
 
@@ -65,14 +67,28 @@ shapesDrawer = {
         ]
       }
 
-      rowsList = [];
-      for (var i = 0; i < rows; i++) {
-        org = {lat: (origin().lat() + i/30000), lng: origin().lng()}
-        rowsList.push(calculateRow(org, cols));
+      function listStartPoints(origin, rows, columns) {
+        rowsList = [];
+        for (var i = 0; i < rows; i++) {
+          org = {lat: (origin.lat() + i/30000), lng: origin.lng()}
+          rowsList.push(calculateRow(org, columns));
+        }
+        return rowsList
       }
 
-      klaxon = rowsList.map(function(startpnt) {return startpnt.map(function(stp) {return boxPoints(stp)})});
+      function makeBoxPoints(startpnts) {
+        return startpnts.map(function(startpnt){
+          return startpnt.map(function(stp) {
+            return boxPoints(stp)}
+          )}
+        )
+      }
+
+
+
+      klaxon = makeBoxPoints(listStartPoints(origin(), rows, cols))
       llll = klaxon.map(function(cls){return cls.map(function(shp){boxCounter.incrementBoxCount(); return drawExistingShapes(shp)})});
+      mrg = [].concat.apply([], llll);
 
       return llll
     }
@@ -80,10 +96,12 @@ shapesDrawer = {
 
 function btnThing() {
   btn = document.getElementById('submitButton');
+  btn2 = document.getElementById('drawExclRect');
   btn.addEventListener('click', drawThing, false);
+  btn2.addEventListener('click', drawDeletingRectangle.toggleDrawing, false);
 }
 
-boxCounter = function(){
+var boxCounter = function(){
   var boxNum = 0;
   return {
     decrementBoxCount: function() {
@@ -106,6 +124,63 @@ boxCounter = function(){
     }
   };
 }();
+
+var drawDeletingRectangle = function() {
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.RECTANGLE,
+    drawingControl: false,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.RECTANGLE
+      ]
+    },
+    rectangleOptions: {
+      fillColor: '#22AA11',
+      fillOpacity: 0.15,
+      strokeColor: '#111',
+      strokeOpacity: 0.9,
+      strokeWeight: 2,
+      clickable: false,
+      editable: false,
+      zIndex: 1
+    }
+  });
+
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+    currentPoly = event.overlay;
+
+    mrg.filter(function(zed) {
+      return zed.getPath().getArray().some(function(pt) {
+        if (currentPoly.getBounds().contains(pt)) {
+          return true
+        } else {
+          return false
+        }
+      })
+    }).forEach(function(splat){splat.setMap()});
+  });
+
+  return {
+    enableDrawing: function() {
+      drawingManager.setMap(map);
+    },
+
+    disableDrawing: function() {
+      drawingManager.setMap();
+    },
+
+    toggleDrawing: function() {
+      if (drawingManager.getMap() === map) {
+        drawingManager.setMap();
+      } else {
+        drawingManager.setMap(map);
+      }
+    }
+
+  }
+}();
+
 
 window.onload = function() {
   btnThing();
